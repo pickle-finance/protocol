a1bb36cc212c868e3f2ed42b7b89a194bf655b30
Shing Ho Tan <talenteyedev@gmail.com>
2021-01-26 06:11:22 -0800

    Make bas-dai strategy Modify bac-dai test for change name

---
+++ src/strategies/basis/strategy-basis-bas-dai-lp.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-basis-farm-base.sol";
+
+contract StrategyBasisBasDaiLp is StrategyBasisFarmBase {
+    // Token addresses
+    address public bas_rewards = 0x9569d4CD7AC5B010DA5697E952EFB1EC0Efb0D0a;
+    address public uni_bas_dai_lp = 0x0379dA7a5895D13037B6937b109fA8607a659ADF;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyBasisFarmBase(
+            bas,
+            bas_rewards,
+            uni_bas_dai_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyBasisBasDaiLp";
+    }
+}
