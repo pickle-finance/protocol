b517a7da9cc0660c52f2d584551e34fc1ec8167a
0xkoffee <0xkoffee@protonmail.com>
2021-02-22 16:57:27 -0400

    Add DILL test env scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,20 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.6.7",
-  hardhat: {
-    forking: {
-      url:
-        "https://eth-mainnet.alchemyapi.io/v2/C4ZFV1uFaAaDsJB8v_dSSCOFFjbnfgtB",
+  networks: {
+    hardhat: {
+      forking: {
+        url:
+          "https://eth-mainnet.alchemyapi.io/v2/C4ZFV1uFaAaDsJB8v_dSSCOFFjbnfgtB",
+        // blockNumber: 11934000,
+      },
+      chainId: 1337,
+      timeout: 100000000,
+    },
+    localhost: {
+      chainId: 1337,
+      url: "http://127.0.0.1:8545",
+      timeout: 100000000,
     },
   },
   paths: {
