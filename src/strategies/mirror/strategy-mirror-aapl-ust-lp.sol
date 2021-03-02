14f6d5f20d320715f21695d36ca818cb2d6d28b6
Manny Reimi <mannyreimi@gmail.com>
2021-03-03 00:05:30 +0800

    Add Mirror mAAPL-UST lp Strategy

---
+++ src/strategies/mirror/strategy-mirror-aapl-ust-lp.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-mirror-farm-base.sol";
+
+contract StrategyMirrorAaplUstLp is StrategyMirFarmBase {
+    // Token addresses
+    address public aapl_rewards = 0x735659C8576d88A2Eb5C810415Ea51cB06931696;
+    address public uni_aapl_ust_lp = 0xB022e08aDc8bA2dE6bA4fECb59C6D502f66e953B;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyMirFarmBase(
+            mir,
+            aapl_rewards,
+            uni_aapl_ust_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyMirrorAaplUstLp";
+    }
+}
