b455695a2ca56705de13a9e9bbc09408ccf654fe
Manny Reimi <mannyreimi@gmail.com>
2021-03-02 22:18:27 +0800

    Add Mirror mQQQ-UST lp Strategy

---
+++ src/strategies/mirror/strategy-mirror-qqq-ust-lp.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-mirror-farm-base.sol";
+
+contract StrategyMirrorQqqUstLp is StrategyMirFarmBase {
+    // Token addresses
+    address public qqq_rewards = 0xc1d2ca26A59E201814bF6aF633C3b3478180E91F;
+    address public uni_qqq_ust_lp = 0x9E3B47B861B451879d43BBA404c35bdFb99F0a6c;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyMirFarmBase(
+            mir,
+            qqq_rewards,
+            uni_qqq_ust_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyMirrorQqqUstLp";
+    }
+}
