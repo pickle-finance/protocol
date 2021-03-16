01b18169edfffe266da782a7e4c093aad715cd6a
AbiEncode <abiencode@gmail.com>
2021-03-16 10:23:27 -0400

    Add unit test for mSLV-UST strategy

--- src/strategies/mirror/strategy-mirror-slv-ust-lp.sol
+++ src/strategies/mirror/strategy-mirror-slv-ust-lp.sol
@@ -7,6 +7,7 @@ contract StrategyMirrorSlvUstLp is StrategyMirFarmBase {
     // Token addresses
     address public slv_rewards = 0xDB278fb5f7d4A7C3b83F80D18198d872Bbf7b923;
     address public uni_slv_ust_lp = 0x860425bE6ad1345DC7a3e287faCBF32B18bc4fAe;
+    address public mSLV = 0x9d1555d8cB3C846Bb4f7D5B1B1080872c3166676;
 
     constructor(
         address _governance,
@@ -16,7 +17,7 @@ contract StrategyMirrorSlvUstLp is StrategyMirFarmBase {
     )
         public
         StrategyMirFarmBase(
-            mir,
+            mSLV,
             slv_rewards,
             uni_slv_ust_lp,
             _governance,
