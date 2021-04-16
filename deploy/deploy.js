d295e014254fb26e79b91f13e5563e41bc8d87c9
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-04-16 16:19:29 -0700

    add deploy scripts and packages

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,5 +1,5 @@
-const governance = "0xD92c7fAa0Ca0e6AE4918f3a83d9832d9CAEAA0d3";
-const strategist = "0x1CbF903De5D688eDa7D6D895ea2F0a8F2A521E99";
+const governance = "0x9d074E37d408542FD38be78848e8814AFB38db17";
+const strategist = "0xaCfE4511CE883C14c4eA40563F176C3C09b4c47C";
 const controller = "0x6847259b2B3A4c17e7c43C54409810aF48bA5210";
 const timelock = "0xD92c7fAa0Ca0e6AE4918f3a83d9832d9CAEAA0d3";
 
@@ -9,13 +9,13 @@ const harvesters = [
   "0xb4522eB2cA49963De9c3dC69023cBe6D53489C98",
 ];
 
-const want = "0x9461173740D27311b176476FA27e94C681b1Ea6b";
+const want = "0x3E78F2E7daDe07ea685F8612F00477FD97162F1e";
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, execute } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy("StrategySushiEthYvBoostLp", {
+  await deploy("StrategyBasisBasDaiLpV3", {
     from: deployer,
     args: [governance, strategist, controller, timelock],
     log: true,
@@ -28,7 +28,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   });
 
   await execute(
-    "StrategySushiEthYvBoostLp",
+    "StrategyBasisBasDaiLpV3",
     { from: deployer, log: true },
     "whitelistHarvesters",
     harvesters
