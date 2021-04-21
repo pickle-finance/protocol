441b1e6b246503e8795b1093dd61faf43841ba18
icpigci <icpigci@gmail.com>
2021-04-22 05:11:44 +1000

    script to grant mint role to masterchef

--- scripts/polygon/deploy.js
+++ scripts/polygon/deploy.js
@@ -7,32 +7,68 @@ const deployPickleToken = async () => {
   const childChainManager = "0x195fe6EE6639665CCeB15BCCeB9980FC445DFa0B";
   const minter = "0xaCfE4511CE883C14c4eA40563F176C3C09b4c47C";
 
-  const PickleTokenFactory = await ethers.getContractFactory("src/polygon/pickle-token.sol:PickleToken");
+  const PickleTokenFactory = await ethers.getContractFactory(
+    "src/polygon/pickle-token.sol:PickleToken"
+  );
   const PickleToken = await PickleTokenFactory.deploy(
-    "PickleToken", "PICKLE", 18, childChainManager, minter
+    "PickleToken",
+    "PICKLE",
+    18,
+    childChainManager,
+    minter
   );
   console.log("pickle token deployed at ", PickleToken.address);
 };
 
 const deployMasterChef = async () => {
   console.log("deploying master chef...");
-  
+
   const pickle = "0x835804CC589E07FBbbCE7B8c830F219Dac407F63";
-  const devaddr = "0xacfe4511ce883c14c4ea40563f176c3c09b4c47c";
+  const devaddr = "0xaCfE4511CE883C14c4eA40563F176C3C09b4c47C";
   const picklePerBlock = 1000000000000;
   const startBlock = 13560000;
   const bonusEndBlock = 0;
 
-  const MasterChefFactory = await ethers.getContractFactory("src/polygon/masterchef.sol:MasterChef");
+  const MasterChefFactory = await ethers.getContractFactory(
+    "src/polygon/masterchef.sol:MasterChef"
+  );
   const MasterChef = await MasterChefFactory.deploy(
-    pickle, devaddr, picklePerBlock, startBlock, bonusEndBlock
+    pickle,
+    devaddr,
+    picklePerBlock,
+    startBlock,
+    bonusEndBlock
   );
   console.log("master chef deployed at ", MasterChef.address);
 };
 
+const handOverPermsToMasterChef = async () => {
+  console.log("grant mint role to master chef...");
+
+  const pickle = "0x835804CC589E07FBbbCE7B8c830F219Dac407F63";
+  const masterchef = "0x52076435D07DDa4c43dD87E76B624c5D0ce4B01D";
+  const DEFAULT_ADMIN_ROLE =
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+  const pickleToken = await ethers.getContractAt(
+    "src/polygon/pickle-token.sol:PickleToken",
+    pickle
+  );
+  const prevAdmin = new ethers.Wallet(
+    "prevAdmin's privatekey here", // 0xaCfE4511CE883C14c4eA40563F176C3C09b4c47C = minter at PickleToken contract deployment.
+    ethers.provider
+  );
+  await pickleToken
+    .connect(prevAdmin)
+    .grantRole(DEFAULT_ADMIN_ROLE, masterchef);
+
+  console.log("mint role granted!");
+};
+
 const main = async () => {
   // await deployPickleToken();
-  await deployMasterChef();
+  // await deployMasterChef();
+  await handOverPermsToMasterChef();
 };
 
 main()
