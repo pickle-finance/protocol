cf372c8be52ea3e45d52013658c87b0a9d78ab2b
icpigci <icpigci@gmail.com>
2021-04-22 04:42:37 +1000

    add masterchef deployment script

--- scripts/polygon/deploy.js
+++ scripts/polygon/deploy.js
@@ -7,7 +7,6 @@ const deployPickleToken = async () => {
   const childChainManager = "0x195fe6EE6639665CCeB15BCCeB9980FC445DFa0B";
   const minter = "0xaCfE4511CE883C14c4eA40563F176C3C09b4c47C";
 
-  console.log(await ethers.provider.getNetwork());
   const PickleTokenFactory = await ethers.getContractFactory("src/polygon/pickle-token.sol:PickleToken");
   const PickleToken = await PickleTokenFactory.deploy(
     "PickleToken", "PICKLE", 18, childChainManager, minter
@@ -15,8 +14,25 @@ const deployPickleToken = async () => {
   console.log("pickle token deployed at ", PickleToken.address);
 };
 
+const deployMasterChef = async () => {
+  console.log("deploying master chef...");
+  
+  const pickle = "0x835804CC589E07FBbbCE7B8c830F219Dac407F63";
+  const devaddr = "0xacfe4511ce883c14c4ea40563f176c3c09b4c47c";
+  const picklePerBlock = 1000000000000;
+  const startBlock = 13560000;
+  const bonusEndBlock = 0;
+
+  const MasterChefFactory = await ethers.getContractFactory("src/polygon/masterchef.sol:MasterChef");
+  const MasterChef = await MasterChefFactory.deploy(
+    pickle, devaddr, picklePerBlock, startBlock, bonusEndBlock
+  );
+  console.log("master chef deployed at ", MasterChef.address);
+};
+
 const main = async () => {
-  await deployPickleToken();
+  // await deployPickleToken();
+  await deployMasterChef();
 };
 
 main()
