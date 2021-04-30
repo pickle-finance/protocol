e43b5baa1a2d6aeb484ee3be42b19737ca0e7933
Kevin McIntyre <kebin70@gmail.com>
2021-04-30 02:02:02 +0000

    add ids

--- contracts/strategies/pangolin/strategy-png-wavax-yfi-lp.sol
+++ contracts/strategies/pangolin/strategy-png-wavax-yfi-lp.sol
@@ -5,9 +5,9 @@ import "../strategy-png-farm-base.sol";
 
 contract StrategyPngAvaxYfiLp is StrategyPngFarmBase {
     // Token addresses
-    address public png_avax_yfi_lp_rewards = ;
+    address public png_avax_yfi_lp_rewards = 0x4dF32F1F8469648e89E62789F4246f73fe768b8E;
     address public png_avax_yfi_lp = 0x7A886B5b2F24eD0Ec0B3C4a17b930E16d160BD17;
-    address public yfi = ;
+    address public yfi = 0x99519AcB025a0e0d44c3875A4BbF03af65933627;
 
     constructor(
         address _governance,
