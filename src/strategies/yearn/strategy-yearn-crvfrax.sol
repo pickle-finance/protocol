51cc478bbe4d87510272bea3b478c7f14d13c598
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-05-07 12:49:55 -0700

    fix and deploy yearn steth strategy + jar

--- src/strategies/yearn/strategy-yearn-crvfrax.sol
+++ src/strategies/yearn/strategy-yearn-crvfrax.sol
@@ -6,7 +6,7 @@ import "../strategy-yearn-affiliate.sol";
 contract StrategyYearnCrvFrax is StrategyYearnAffiliate {
     // Token addresses
     address public crv_frax_lp = 0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B;
-    address public yearn_registry = 0x3eE41C098f9666ed2eA246f4D2558010e59d63A0
+    address public yearn_registry = 0x3eE41C098f9666ed2eA246f4D2558010e59d63A0;
 
     constructor(
         address _governance,
@@ -24,10 +24,4 @@ contract StrategyYearnCrvFrax is StrategyYearnAffiliate {
             _timelock
         )
     {}
-
-    // **** Views ****
-
-    function getName() external override pure returns (string memory) {
-        return "StrategyYearnCrvFrax";
-    }
 }
--- src/strategies/yearn/strategy-yearn-crvlusd.sol
+++ src/strategies/yearn/strategy-yearn-crvlusd.sol
@@ -6,7 +6,7 @@ import "../strategy-yearn-affiliate.sol";
 contract StrategyYearnCrvLusd is StrategyYearnAffiliate {
     // Token addresses
     address public crv_lusd_lp = 0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA;
-    address public yearn_registry = 0x3eE41C098f9666ed2eA246f4D2558010e59d63A0
+    address public yearn_registry = 0x3eE41C098f9666ed2eA246f4D2558010e59d63A0;
 
     constructor(
         address _governance,
@@ -24,10 +24,4 @@ contract StrategyYearnCrvLusd is StrategyYearnAffiliate {
             _timelock
         )
     {}
-
-    // **** Views ****
-
-    function getName() external override pure returns (string memory) {
-        return "StrategyYearnCrvLusd";
-    }
 }
