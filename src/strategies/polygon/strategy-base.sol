916f3a457bca4833aad549988bffb4eb6f20cc44
yiepier <garmoniart@gmail.com>
2021-05-27 14:27:03 -0400

    Add sushi weth/usdt strategy

--- src/strategies/polygon/strategy-base.sol
+++ src/strategies/polygon/strategy-base.sol
@@ -35,6 +35,7 @@ abstract contract StrategyBase {
     // Tokens
     address public want;
     address public constant weth = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+    address public constant wmatic = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
 
     // User accounts
     address public governance;
@@ -44,6 +45,7 @@ abstract contract StrategyBase {
 
     // Dex
     address public univ2Router2 = 0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25;
+    address public sushiRouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
 
     mapping(address => bool) public harvesters;
 
@@ -303,6 +305,50 @@ abstract contract StrategyBase {
         require(path[1] != address(0));
 
         UniswapRouterV2(univ2Router2).swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this),
+            now.add(60)
+        );
+    }
+
+    function _swapSushiswap(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        require(_to != address(0));
+
+        address[] memory path;
+
+        if (_from == weth || _to == weth) {
+            path = new address[](2);
+            path[0] = _from;
+            path[1] = _to;
+        } else {
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = weth;
+            path[2] = _to;
+        }
+
+        UniswapRouterV2(sushiRouter).swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this),
+            now.add(60)
+        );
+    }
+
+    function _swapSushiswapWithPath(
+        address[] memory path,
+        uint256 _amount
+    ) internal {
+        require(path[1] != address(0));
+
+        UniswapRouterV2(sushiRouter).swapExactTokensForTokens(
             _amount,
             0,
             path,
---
+++ src/tests/strategies/polygon/sushiswap/strategy-polygon-sushi-eth-usdt-lp.test.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.6.7;
+
+
+
+import "../../../lib/polygon/sushiswap/test-strategy-sushi-farm-base.sol";
+
+import "../../../../interfaces/strategy.sol";
+import "../../../../interfaces/curve.sol";
+import "../../../../interfaces/uniswapv2.sol";
+
+import "../../../../pickle-jar.sol";
+import "../../../../controller-v4.sol";
+import "../../../../strategies/polygon/sushiswap/strategy-sushi-eth-usdt-lp.sol";
+
+contract StrategySushiEthUsdtLpTest is StrategySushiFarmTestBase {
+    function setUp() public {
+        want = 0xc2755915a85C6f6c1C0F3a86ac8C058F11Caa9C9;
+
+        governance = address(this);
+        strategist = address(this);
+        devfund = address(new User());
+        treasury = address(new User());
+        timelock = address(this);
+
+        controller = new ControllerV4(
+            governance,
+            strategist,
+            timelock,
+            devfund,
+            treasury
+        );
+
+        strategy = IStrategy(
+            address(
+                new StrategySushiEthUsdtLp(
+                    governance,
+                    strategist,
+                    address(controller),
+                    timelock
+                )
+            )
+        );
+
+        pickleJar = new PickleJar(
+            strategy.want(),
+            governance,
+            timelock,
+            address(controller)
+        );
+
+        controller.setJar(strategy.want(), address(pickleJar));
+        controller.approveStrategy(strategy.want(), address(strategy));
+        controller.setStrategy(strategy.want(), address(strategy));
+
+        // Set time
+        hevm.warp(startTime);
+    }
+
+    // **** Tests ****
+
+    function test_ethusdtv3_1_timelock() public {
+        _test_timelock();
+    }
+
+    function test_ethusdtv3_1_withdraw_release() public {
+        _test_withdraw_release();
+    }
+
+    function test_ethusdtv3_1_get_earn_harvest_rewards() public {
+        _test_get_earn_harvest_rewards();
+    }
+}
