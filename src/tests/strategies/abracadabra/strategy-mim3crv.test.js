556d0dccd705dd3196179df92a52e05e7f088a3b
abiencode <abiencode@gmail.com>
2021-07-22 04:42:11 -0700

    Add mim3crv strategy

---
+++ src/tests/strategies/abracadabra/strategy-mim3crv.test.js
@@ -0,0 +1,17 @@
+const {toWei} = require("../../utils/testHelper");
+const {getWantFromWhale} = require("../../utils/setupHelper");
+const {doTestBehaviorBase} = require("../testBehaviorBase");
+const {UNI_ROUTER} = require("../../utils/constants");
+
+describe("StrategyMim3Crv", () => {
+  const want_addr = "0x5a6A4D54456819380173272A5E8E9B9904BdF41B";
+
+  const whale_addr = "0x1889AF6eB9Bd12fA90FDB7A4C857Eff510d54530";
+
+  before("Get want token", async () => {
+    [alice] = await hre.ethers.getSigners();
+    await getWantFromWhale(want_addr, toWei(1000), alice, whale_addr);
+  });
+
+  doTestBehaviorBase("StrategyMim3crvLp", want_addr);
+});
