128f4d5f066edbcfec7df6ab21e9e540798eb254
abiencode <abiencode@gmail.com>
2021-07-26 19:44:47 -0700

    update withdrawsaddle unit test

--- src/strategies/saddle/withdraw-saddle.sol
+++ src/strategies/saddle/withdraw-saddle.sol
@@ -1,17 +1,22 @@
 /**
  *Submitted for verification at Etherscan.io on 2021-04-29
-*/
+ */
 
 pragma solidity ^0.6.7;
+pragma experimental ABIEncoderV2;
+
 import "../../interfaces/saddle-farm.sol";
-import "../../interfaces/erc20.sol";
+import "../../lib/erc20.sol";
+import "../../lib/safe-math.sol";
 
 contract WithdrawSaddle {
-  
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
     address public staking = 0x0639076265e9f88542C91DCdEda65127974A5CA5;
     address public want = 0xd48cF4D7FB0824CC8bAe055dF3092584d0a1726A;
     address public saddle_strategy = 0x4A974495E20A8E0f5ce1De59eB15CfffD19Bcf8d;
-    address public governance = 0xacfe4511ce883c14c4ea40563f176c3c09b4c47c;
+    address public governance = 0xaCfE4511CE883C14c4eA40563F176C3C09b4c47C;
 
     function withdraw(uint256 _amount) external {
         LockedStake[] memory lockedStakes = ICommunalFarm(staking)
