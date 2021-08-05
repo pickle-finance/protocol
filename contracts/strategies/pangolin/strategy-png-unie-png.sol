94bbdb4d2d45a65457767df89503aa114ccf14c7
Jomari Peterson <jomari.peterson@gmail.com>
2021-08-04 18:47:00 -0700

    png aave uni and yfi

---
+++ contracts/strategies/pangolin/strategy-png-unie-png.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-png-farm-base-png.sol";
+
+contract StrategyPngUniPngLp is StrategyPngFarmBasePng {
+    // Token addresses
+    address public png_uni_png_lp_rewards = 0xD4E49A8Ec23daB51ACa459D233e9447DE03AFd29;
+    address public png_uni_png_lp = 0x792828974273725A7027da1C2341f4162e17174b;
+    address public uni = 0x8eBAf22B6F053dFFeaf46f4Dd9eFA95D89ba8580;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyPngFarmBasePng(
+            uni,
+            png_uni_png_lp_rewards,
+            png_uni_png_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyPngUniPngLp";
+    }
+}
