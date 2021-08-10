ae8a18d7640981a83936bf289d319f4b88a4e6b7
Jomari Peterson <jomari.peterson@gmail.com>
2021-08-09 22:13:53 -0700

    Initial Commit for xJoe Strategy

---
+++ contracts/strategies/strategy-xjoe-farm-base.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "./strategy-base.sol";
+import "../interfaces/masterchefjoev2.sol";
+import "../interfaces/joe-rewarder.sol";
+import "../interfaces/joe.sol";
+import "../interface/IJoeBar.sol";
+
+abstract contract StrategyxJoeFarmBase is StrategyBase {
+    // Token addresses
+    address public constant joe = 0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd;
+    address public constant joeRouter = 0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
+	address public constant joeBar = 0x57319d41F71E81F3c65F2a47CA4e001EbAFd4F33;
+
+    address public constant masterChefJoeV2 = 0xd6a4F121CA35509aF06A0Be99093d08462f53052;
+
+    uint256 public poolId;
+
+    // How much PNG tokens to keep?
+    uint256 public keepJOE = 0;
+    uint256 public constant keepJOEMax = 10000;
+
+    constructor(
+        uint256 _poolId,
+        address _lp,
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyBase(_lp, _governance, _strategist, _controller, _timelock)
+    {
+        poolId = _poolId;
+    }
+
+    function balanceOfPool() public view override returns (uint256) {
+        (uint256 amount, ) =
+            IMasterChefJoeV2(masterChefJoeV2).userInfo(poolId, address(this));
+        return amount;
+    }
+
+    // Updated based on cryptofish's recommendation
+    function getHarvestable() external view returns (uint256, uint256) {
+        (uint256 pendingJoe, , , uint256 pendingBonusToken) = IMasterChefJoeV2(masterChefJoeV2).pendingTokens(poolId, address(this));
+        return (pendingJoe, pendingBonusToken);
+    }
+
+    // **** Setters ****
+    function deposit() public override {
+        uint256 _want = IERC20(want).balanceOf(address(this));
+        if (_want > 0) {
+            IERC20(want).safeApprove(masterChefJoeV2, 0);
+            IERC20(want).safeApprove(masterChefJoeV2, _want);
+            IMasterChefJoeV2(masterChefJoeV2).deposit(poolId, _want);
+        }
+    }
+
+    function _withdrawSome(uint256 _amount)
+        internal
+        override
+        returns (uint256)
+    {
+        IMasterChefJoeV2(masterChefJoeV2).withdraw(poolId, _amount);
+        return _amount;
+    }
+
+    function _swapTraderJoe(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        require(_to != address(0));
+
+        address[] memory path;
+
+        if (_from == wavax || _to == wavax) {
+            path = new address[](2);
+            path[0] = _from;
+            path[1] = _to;
+        } else {
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = wavax;
+            path[2] = _to;
+        }
+
+        IJoeRouter(joeRouter).swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this),
+            now.add(60)
+        );
+    }
+
+    function _swapTraderJoeWithPath(
+        address[] memory path,
+        uint256 _amount
+    ) internal {
+        require(path[1] != address(0));
+
+        IJoeRouter(joeRouter).swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this),
+            now.add(60)
+        );
+    }
+}
---
+++ contracts/strategies/traderJoe/strategy-joe-x-joe.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-xjoe-farm-base.sol";
+
+contract StrategyJoexJoe is StrategyxJoeFarmBase {
+
+    uint256 public avax_snob_poolId = 8;
+
+    address public joe_avax_snob_lp = 0x8fB5bD3aC8eFD05DACae82F512dD03e14aAdAb73;
+    address public snob = 0xC38f41A296A4493Ff429F1238e030924A1542e50;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyxJoeFarmBase(
+            avax_snob_poolId,
+            joe_avax_snob_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** State Mutations ****
+
+    function harvest() public override onlyBenevolent {
+        // Anyone can harvest it at any given time.
+        // I understand the possibility of being frontrun
+        // But AVAX is a dark forest, and I wanna see how this plays out
+        // i.e. will be be heavily frontrunned?
+        //      if so, a new strategy will be deployed.
+
+        // Collects Joe tokens
+        IMasterChefJoeV2(masterChefJoeV2).deposit(poolId, 0);
+
+        uint256 _joe = IERC20(joe).balanceOf(address(this));
+        if (_joe > 0) {
+            // 10% is sent to treasury
+            uint256 _keepJOE = _joe.mul(keepJOE).div(keepJOEMax);
+            IERC20(joe).safeTransfer(
+                IController(controller).treasury(),
+                _keepJOE
+            );
+            uint256 _amount = _joe.sub(_keepJOE).div(2);
+            IERC20(joe).safeApprove(joeRouter, 0);
+            IERC20(joe).safeApprove(joeRouter, _joe.sub(_keepJOE));
+
+            _swapTraderJoe(joe, wavax, _amount);
+            _swapTraderJoe(joe, snob, _amount);
+        }
+
+        // Adds in liquidity for AVAX/SNOB
+        uint256 _wavax = IERC20(wavax).balanceOf(address(this));
+
+        uint256 _snob = IERC20(snob).balanceOf(address(this));
+
+        if (_wavax > 0 && _snob > 0) {
+            IERC20(wavax).safeApprove(joeRouter, 0);
+            IERC20(wavax).safeApprove(joeRouter, _wavax);
+
+            IERC20(snob).safeApprove(joeRouter, 0);
+            IERC20(snob).safeApprove(joeRouter, _snob);
+
+            IJoeRouter(joeRouter).addLiquidity(
+                wavax,
+                snob,
+                _wavax,
+                _snob,
+                0,
+                0,
+                address(this),
+                now + 60
+            );
+
+            // Donates DUST
+            IERC20(wavax).transfer(
+                IController(controller).treasury(),
+                IERC20(wavax).balanceOf(address(this))
+            );
+            IERC20(snob).safeTransfer(
+                IController(controller).treasury(),
+                IERC20(snob).balanceOf(address(this))
+            );
+        }
+
+        _distributePerformanceFeesAndDeposit();
+    }
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyJoeAvaxSnobLp";
+    }
+}
