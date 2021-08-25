9c222fe7a0198f0fa8688eb6180c35aa6f3eb71c
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-08-25 16:31:50 -0400

    modify mim strat to compound SPELL

--- src/strategies/convex/strategy-convex-mim3crv.sol
+++ src/strategies/convex/strategy-convex-mim3crv.sol
@@ -14,6 +14,7 @@ contract StrategyConvexMim3Crv is StrategyConvexFarmBase {
     address public lpToken = 0x5a6A4D54456819380173272A5E8E9B9904BdF41B;
     uint256 public crvTricryptoPoolId = 40;
     address public mim = 0x99D8a9C45b2ecA8864373A26D1459e3Dff1e17F3;
+    address public spell = 0x090185f2135308BaD17527004364eBcC2D37e5F6;
     address public zapper = 0xA79828DF1850E8a3A3064576f380D90aECDD3359;
 
     constructor(
@@ -33,42 +34,71 @@ contract StrategyConvexMim3Crv is StrategyConvexFarmBase {
         )
     {}
 
-    function getName() external pure override returns (string memory) {
+    function getName() external override pure returns (string memory) {
         return "StrategyConvexMim3Crv";
     }
 
-    function getHarvestable() public view returns (uint256, uint256) {
-        return (get_crv_earned(), get_cvx_earned());
+    function get_spell_earned() public view returns (uint256) {
+        return
+            IVirtualBalanceRewardPool(
+                IBaseRewardPool(getCrvRewardContract()).extraRewards(0)
+            )
+                .earned(address(this));
+    }
+
+    function getHarvestable()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (get_crv_earned(), get_cvx_earned(), get_spell_earned());
     }
 
     function harvest() public override onlyBenevolent {
-        (uint256 _crvHarvestable, uint256 _cvxHarvestable) = getHarvestable();
+        (
+            uint256 _crvHarvestable,
+            uint256 _cvxHarvestable,
+            uint256 _spellHarvestable
+        ) = getHarvestable();
 
-        if (_crvHarvestable > 0 || _cvxHarvestable > 0)
+        if (_crvHarvestable > 0 || _cvxHarvestable > 0 || _spellHarvestable > 0)
             IBaseRewardPool(getCrvRewardContract()).getReward(
                 address(this),
                 true
             );
 
+        address[] memory path = new address[](3);
+        path[1] = weth;
+        path[2] = mim;
+
         uint256 _cvx = IERC20(cvx).balanceOf(address(this));
         if (_cvx > 0) {
             IERC20(cvx).safeApprove(sushiRouter, 0);
             IERC20(cvx).safeApprove(sushiRouter, _cvx);
-            _swapSushiswap(cvx, weth, _cvx);
+            path[0] = cvx;
+            _swapSushiswapWithPath(path, _cvx);
         }
+
         uint256 _crv = IERC20(crv).balanceOf(address(this));
 
         if (_crv > 0) {
             IERC20(crv).safeApprove(sushiRouter, 0);
             IERC20(crv).safeApprove(sushiRouter, _crv);
-            _swapSushiswap(crv, weth, _crv);
+            path[0] = crv;
+            _swapSushiswapWithPath(path, _crv);
         }
 
-        uint256 _weth = IERC20(weth).balanceOf(address(this));
-        if (_weth > 0) {
-            IERC20(weth).safeApprove(sushiRouter, 0);
-            IERC20(weth).safeApprove(sushiRouter, _weth);
-            _swapSushiswap(weth, mim, _weth);
+        uint256 _spell = IERC20(spell).balanceOf(address(this));
+
+        if (_spell > 0) {
+            IERC20(spell).safeApprove(sushiRouter, 0);
+            IERC20(spell).safeApprove(sushiRouter, _spell);
+            path[0] = spell;
+            _swapSushiswapWithPath(path, _spell);
         }
 
         uint256 _mim = IERC20(mim).balanceOf(address(this));
