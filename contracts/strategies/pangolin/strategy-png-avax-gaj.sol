b2b295437378399374e46b4263e3b234db5ab3d6
Jomari Peterson <jomari.peterson@gmail.com>
2021-08-25 20:09:11 -0700

    Pangolin Pairs

---
+++ contracts/strategies/pangolin/strategy-png-avax-gaj.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-png-farm-base.sol";
+
+contract StrategyPngAvaxHuskyLp is StrategyPngFarmBase {
+    // Token addresses
+    address public png_avax_husky_lp_rewards = 0x2e60ab79bbcdfea164874700d5d98969a386eb2a;
+    address public png_avax_husky_lp = 0xd05e435Ae8D33faE82E8A9E79b28aaFFb54c1751;
+    address public husky = 0x65378b697853568dA9ff8EaB60C13E1Ee9f4a654;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyPngFarmBase(
+            husky,
+            png_avax_husky_lp_rewards,
+            png_avax_husky_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyPngAvaxHuskyLp";
+    }
+}
---
+++ contracts/strategies/pangolin/strategy-png-avax-lyd.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-png-farm-base.sol";
+
+contract StrategyPngAvaxLydLp is StrategyPngFarmBase {
+    // Token addresses
+    address public png_avax_lyd_lp_rewards = 0xe6de666a80a357497a2cab3a91f1c28dcaa1eca4;
+    address public png_avax_lyd_lp = 0x87B1Cf8f0Fd3e0243043642Cea7164a67Cb67E4d;
+    address public lyd = 0x4C9B4E1AC6F24CdE3660D5E4Ef1eBF77C710C084;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyPngFarmBase(
+            lyd,
+            png_avax_lyd_lp_rewards,
+            png_avax_lyd_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyPngAvaxLydLp";
+    }
+}
