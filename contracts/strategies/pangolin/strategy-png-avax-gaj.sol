91f77176e398f45a56cc049cb03e3d6e2866ab69
Jomari Peterson <jomari.peterson@gmail.com>
2021-08-25 20:09:40 -0700

    Update strategy-png-avax-gaj.sol

--- contracts/strategies/pangolin/strategy-png-avax-gaj.sol
+++ contracts/strategies/pangolin/strategy-png-avax-gaj.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.6.7;
 
 import "../strategy-png-farm-base.sol";
 
-contract StrategyPngAvaxHuskyLp is StrategyPngFarmBase {
+contract StrategyPngAvaxGajLp is StrategyPngFarmBase {
     // Token addresses
-    address public png_avax_husky_lp_rewards = 0x2e60ab79bbcdfea164874700d5d98969a386eb2a;
-    address public png_avax_husky_lp = 0xd05e435Ae8D33faE82E8A9E79b28aaFFb54c1751;
-    address public husky = 0x65378b697853568dA9ff8EaB60C13E1Ee9f4a654;
+    address public png_avax_gaj_lp_rewards = 0xd31ffd05a41645631a22a64c1f870a6248a4ddcf;
+    address public png_avax_gaj_lp = 0x278f24A782B96BE10f15df93487Aec5331CfdFF1;
+    address public gaj = 0x595c8481c48894771CE8FaDE54ac6Bf59093F9E8;
 
     constructor(
         address _governance,
@@ -17,9 +17,9 @@ contract StrategyPngAvaxHuskyLp is StrategyPngFarmBase {
     )
         public
         StrategyPngFarmBase(
-            husky,
-            png_avax_husky_lp_rewards,
-            png_avax_husky_lp,
+            gaj,
+            png_avax_gaj_lp_rewards,
+            png_avax_gaj_lp,
             _governance,
             _strategist,
             _controller,
@@ -30,6 +30,6 @@ contract StrategyPngAvaxHuskyLp is StrategyPngFarmBase {
     // **** Views ****
 
     function getName() external override pure returns (string memory) {
-        return "StrategyPngAvaxHuskyLp";
+        return "StrategyPngAvaxGajLp";
     }
 }
