3184ffc49cf01a42ba116887a4c2d0ad9df7d90d
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-09-09 02:50:13 -0400

    add test case for multi-jar withdrawals

--- src/tests/strategies/testBehaviorBase.js
+++ src/tests/strategies/testBehaviorBase.js
@@ -12,7 +12,7 @@ const doTestBehaviorBase = (strategyName, want_addr, bIncreaseBlock = false, isP
     before("Setup contracts", async () => {
       [alice, devfund, treasury] = await hre.ethers.getSigners();
       governance = alice;
-      strategist = alice;
+      strategist = devfund;
       timelock = alice;
 
       want = await getContractAt("ERC20", want_addr);
@@ -102,7 +102,10 @@ const doTestBehaviorBase = (strategyName, want_addr, bIncreaseBlock = false, isP
       console.log("💸 Treasury balance after harvest: ", _treasuryAfter.toString());
 
       //20% performance fee is given
-      const earned = _after.sub(_before).mul(1000).div(800);
+      const earned = _after
+        .sub(_before)
+        .mul(1000)
+        .div(800);
       const earnedRewards = earned.mul(200).div(1000);
       const actualRewardsEarned = _treasuryAfter.sub(_treasuryBefore);
       console.log("\nActual reward earned by treasury: ", actualRewardsEarned.toString());
@@ -129,6 +132,51 @@ const doTestBehaviorBase = (strategyName, want_addr, bIncreaseBlock = false, isP
       //0% goes to treasury
       const _treasuryFund = _treasuryAfter.sub(_treasuryBefore);
       expect(_treasuryFund).to.be.eq(0, "treasury've stolen money!!!!");
+    });
+
+    it("Should perform multiple deposits and withdrawals correctly", async () => {
+      const _wantHalved = (await want.balanceOf(alice.address)).div(2);
+      await want.connect(alice).transfer(strategist.address, _wantHalved);
+      await want.connect(alice).approve(pickleJar.address, _wantHalved);
+      await want.connect(strategist).approve(pickleJar.address, _wantHalved);
+
+      console.log("\nAlice starting balance: %s\n", _wantHalved.toString());
+
+      await pickleJar.connect(alice).deposit(_wantHalved);
+      await pickleJar.earn();
+
+      await increaseTime(60 * 60 * 24 * 15); //travel 15 days
+
+      await pickleJar.connect(strategist).deposit(_wantHalved);
+
+      await pickleJar.earn();
+
+      await increaseTime(60 * 60 * 24 * 15); //travel 15 days
+
+      // Alice withdraws half
+      await pickleJar.connect(alice).withdraw(_wantHalved.div(2));
+
+      await pickleJar.earn();
+
+      // Strategist withdraws all
+      await pickleJar.connect(strategist).withdrawAll();
+
+      let _aliceBalanceAfter = await want.balanceOf(alice.address);
+      let _strategistBalanceAfter = await want.balanceOf(strategist.address);
+      console.log("\nAlice balance after half withdrawal: %s\n", _aliceBalanceAfter.toString());
+      console.log("\nStrategist balance after half withdrawal: %s\n", _strategistBalanceAfter.toString());
+
+      expect(_aliceBalanceAfter).to.be.eq(_wantHalved.div(2), "Alice withdrawal amount incorrect");
+
+      expect(_strategistBalanceAfter).to.be.eq(_wantHalved, "Strategist withdrawal amount incorrect");
+
+      // Alice withdraws remainder
+
+      await pickleJar.connect(alice).withdrawAll();
+      _aliceBalanceAfter = await want.balanceOf(alice.address);
+      console.log("\nAlice balance after full withdrawal: %s\n", _aliceBalanceAfter.toString());
+      expect(_aliceBalanceAfter).to.be.eq(_wantHalved, "Alice withdrawal amount incorrect");
+
     });
 
     beforeEach(async () => {
