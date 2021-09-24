046c825909acf862a8344b852940308a4fca42ac
Jonas <20801365+Jonasslv@users.noreply.github.com>
2021-09-24 19:25:46 -0300

    Only try deleverage if there's something borrowed

--- contracts/strategies/benqi/strategy-benqi-qi.sol
+++ contracts/strategies/benqi/strategy-benqi-qi.sol
@@ -421,12 +421,14 @@ contract StrategyBenqiQi is StrategyBase, Exponential {
 
             // If the amount we need to free is > borrowed
             // Just free up all the borrowed amount
-            if (borrowedToBeFree > borrowed) {
-                this.deleverageToMin();
-            } else if (borrowed > 0) {
-                 // Just keep freeing up borrowed amounts until
-                // we hit a safe number to redeem our underlying
-                this.deleverageUntil(supplied.sub(borrowedToBeFree));
+            if (borrowed > 0) {
+                if (borrowedToBeFree > borrowed) {
+                    this.deleverageToMin();
+                } else {
+                    // Just keep freeing up borrowed amounts until
+                    // we hit a safe number to redeem our underlying
+                    this.deleverageUntil(supplied.sub(borrowedToBeFree));
+                }
             }
 	    
             // Redeems underlying
