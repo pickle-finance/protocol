73d3b7867ae8a783ec6342cd08b190e3eb980e12
timbotro <timbrinded@hotmail.com>
2021-10-13 13:03:50 +0100

    test fixes

--- contracts/strategies/benqi/strategy-benqi-wavax.sol
+++ contracts/strategies/benqi/strategy-benqi-wavax.sol
@@ -24,24 +24,18 @@ contract StrategyBenqiAvax is StrategyQiFarmBase {
         )
     {}
 
-     function depositQIAvax() public  payable {
+    function deposit() public override {
         uint256 _want = IERC20(want).balanceOf(address(this));
-        
-            require(_want > 0, "Balance of strategy empty");
+        if (_want > 0) {
             //unwrap wavax to avax for benqi
-               WAVAX(want).withdraw(_want);
-            //  confirm that msg.sender received avax
-             require(address(this).balance >= _want, "!unwrap unsuccessful");
-           
-            // mint qiTokens external payable
-              IQiAvax(qiavax).mint{value: _want}();
-              
-            //confirm that qiTokens is received in exchange
-          require( IQiToken(qiavax).balanceOf(address(this)) > 0 , "qitokens not received" );
-            
+            WAVAX(want).withdraw(_want);
+            //make sure the contract address receives avax
+            IERC20(want).safeApprove(qiavax, 0);
+            IERC20(want).safeApprove(qiavax, _want);
+            //IqiToken.mint external payable
+            require(IQiToken(qiavax).mint(_want) == 0, "!deposit");
+        }
     }
-
-
     function _withdrawSome(uint256 _amount)
         internal
         override
@@ -50,16 +44,17 @@ contract StrategyBenqiAvax is StrategyQiFarmBase {
         uint256 _want = balanceOfWant();
         if (_want < _amount) {
             uint256 _redeem = _amount.sub(_want);
+
             //unwrap wavax to avax for benqi
             WAVAX(want).withdraw(_redeem);
             // Make sure market can cover liquidity
             require(IQiToken(qiavax).getCash() >= _redeem, "!cash-liquidity");
+
             // How much borrowed amount do we need to free?
             uint256 borrowed = getBorrowed();
             uint256 supplied = getSupplied();
             uint256 curLeverage = getCurrentLeverage();
             uint256 borrowedToBeFree = _redeem.mul(curLeverage).div(1e18);
-
             // If the amount we need to free is > borrowed
             // Just free up all the borrowed amount
             if (borrowedToBeFree > borrowed) {
@@ -69,13 +64,15 @@ contract StrategyBenqiAvax is StrategyQiFarmBase {
                 // we hit a safe number to redeem our underlying
                 this.deleverageUntil(supplied.sub(borrowedToBeFree));
             }
+
             // Redeems underlying
             require(IQiToken(qiavax).redeemUnderlying(_redeem) == 0, "!redeem");
-             //wrap avax to wavax
-             WAVAX(wavax).deposit{value: _redeem}();
-             //confirm contract address now holds enough wavax;
-             require(IERC20(want).balanceOf(address(this)) >= _amount, "!NotEnoughWavax");
+            //wrap avax to wavax
+            WAVAX(want).deposit();
+            //confirm contract address now holds enough wavax;
+            require(IERC20(want).balanceOf(address(this)) >= _amount, "!NotEnoughWavax");
         }
+
         return _amount;
     }
 
--- test/FoldingStrats/bankerJoe.js
+++ test/FoldingStrats/bankerJoe.js
@@ -9,39 +9,39 @@ const tests = [
   {
     name: "AaveDai",
     tokenAddress: "0xd586E7F844cEa2F87f50152665BCbc2C279D8d70",
-    strategyAddress: "0xfc26ec0c916b9f573bbdfd1eda87d5192339bd5b",
-    snowglobeAddress: "0xE4543C234D4b0aD6d29317cFE5fEeCAF398f5649",
+    strategyAddress: "0xcd9aa4d1a0cee1d0ed3798ded6fb925cbfe598a0",
+    snowglobeAddress: "0x7b5ffcf45193986b757986379628432d90f20aab",
   },
   {
     name: "AaveUsdc",
     tokenAddress: "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
-    strategyAddress: "0xfce6dee4805df0c8bb981549e92922485c90861e",
-    snowglobeAddress: "0x0c33d6076F0Dce93db6e6103E98Ad951A0F33917",
+    strategyAddress: "0xcd4a6733d1e497672290b0c4b891dfc10e03e973",
+    snowglobeAddress: "0x8c9faebd41c68b801d628902edad43d88e4dd0a6",
   },
   {
     name: "AaveUsdt",
     tokenAddress: "0xc7198437980c041c805A1EDcbA50c1Ce5db95118",
-    strategyAddress: "0x1c670e7d2b294e24f71d61f7e0abf5d51fad69fe",
-    snowglobeAddress: "0x567350328dB688d49284e79F7DBfad2AAd094B7A",
+    strategyAddress: "0x3ef4dc17b344cd234fa264d8d0be424207f07532",
+    snowglobeAddress: "0xc7ca863275b2d0f7a07ca6e2550504362705aa1a",
   },
   {
     name: "AaveWavax",
     tokenAddress: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
-    strategyAddress: "0xefb83d176c6632cf787214b7e130aaca99d936ff",
-    snowglobeAddress: "0x951f6c751A9bC5A75a4E4d43be205aADa709D3B8",
+    strategyAddress: "",
+    snowglobeAddress: "",
     slot: 3
   },
   {
     name: "AaveWbtc",
     tokenAddress: "0x50b7545627a5162F82A992c33b87aDc75187B218",
-    strategyAddress: "0x569b2b8254b6887c6a9f310de220506c8e0e2256",
-    snowglobeAddress: "0xcB707aA965aEB9cB03d21dFADf496e6581Cd7b96",
+    strategyAddress: "",
+    snowglobeAddress: "",
   },
   {
     name: "AaveWeth",
     tokenAddress: "0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB",
-    strategyAddress: "0x297991e25f6c4ecb3d10e9c6ee55767d6b727b8c",
-    snowglobeAddress: "0x72b7AddaeFE3e4b6452CFAEcf7C0d11e5EBD05a0",
+    strategyAddress: "",
+    snowglobeAddress: "",
   },
 ];
 
@@ -57,7 +57,7 @@ for (const test of tests) {
       test.amount,
       test.slot,
       test.fold,
-      "aave"
+      "bankerJoe"
       );
   });
 }
