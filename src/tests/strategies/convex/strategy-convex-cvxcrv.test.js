43e8da2504e22a0ef042b815b08749d99e7bf331
Larry the Cucumber <larrythecucumber321@gmail.com>
2021-10-12 23:50:16 -0700

    change naming

--- src/tests/strategies/convex/strategy-convex-cvxcrv.test.js
+++ src/tests/strategies/convex/strategy-convex-cvxcrv.test.js
@@ -2,14 +2,14 @@ const {toWei} = require("../../utils/testHelper");
 const {getWantFromWhale} = require("../../utils/setupHelper");
 const {doTestBehaviorBase} = require("../testBehaviorBase");
 
-describe("StrategyConvexCvxCrvLp", () => {
-  const want_addr = "0x9D0464996170c6B9e75eED71c68B99dDEDf279e8";
-  const whale_addr = "0xfbd50c82ea05d0d8b6b302317880060bc3086866";
+describe("StrategyConvexCvxCrv", () => {
+  const want_addr = "0x62B9c7356A2Dc64a1969e19C23e4f579F9810Aa7";
+  const whale_addr = "0x00f282c40b92bed05f1776cadf1c8b96b9fbaee3";
 
   before("Get want token", async () => {
     [alice] = await hre.ethers.getSigners();
     await getWantFromWhale(want_addr, toWei(1000), alice, whale_addr);
   });
 
-  doTestBehaviorBase("StrategyConvexCvxCrvLp", want_addr);
+  doTestBehaviorBase("StrategyConvexCvxCrv", want_addr);
 });
