c7ec95998b12ed082626aa691e410adf33fa6379
Auroter <auroter34@gmail.com>
2021-10-13 20:46:56 -0700

    Modified Big Green Button to handle governance, timelock, strategist, controller

--- contracts/big-green-button.sol
+++ contracts/big-green-button.sol
@@ -13,15 +13,45 @@ contract BigGreenButton {
 
     address controller;
 
+    address public governance;
+    address public strategist;
+    address public timelock;
+
     constructor(
         address _controller
+        address _governance,
+        address _strategist,
+        address _timelock,
     ) public {
+        governance = _governance;
+        strategist = _strategist;
+        timelock = _timelock;
         controller = _controller;
+    }
+
+    function setController(address _controller) public {
+        require(msg.sender == governance, "!governance");
+        controller = _controller;
+    }
+
+    function setStrategist(address _strategist) public {
+        require(msg.sender == governance, "!governance");
+        strategist = _strategist;
+    }
+
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function setTimelock(address _timelock) public {
+        require(msg.sender == timelock, "!timelock");
+        timelock = _timelock;
     }
 
     function setGlobes(address[] _tokens, address[] _globes) public {
         require(
-            msg.sender == IController(controller).strategist() || msg.sender == IController(controller).governance(),
+            msg.sender == strategist || msg.sender == governance,
             "!strategist"
         );
         require(_tokens.length == _globes.length, "!length");
@@ -31,7 +61,7 @@ contract BigGreenButton {
     }
 
     function approveStrategies(address[] _tokens, address[] _strategies) public {
-        require(msg.sender == IController(controller).timelock(), "!timelock");
+        require(msg.sender == timelock, "!timelock");
         require(_tokens.length == _strategies.length, "!length");
         for (uint256 i = 0; i < _tokens.length; i++) {
             IController(controller).approveStrategy(_tokens[i], _strategies[i]);
@@ -39,7 +69,7 @@ contract BigGreenButton {
     }
 
     function revokeStrategies(address[] _tokens, address[] _strategies) public {
-        require(msg.sender == IController(controller).governance(), "!governance");
+        require(msg.sender == governance, "!governance");
         require(_tokens.length == _strategies.length, "!length");
         for (uint256 i = 0; i < _tokens.length; i++) {
             IController(controller).revokeStrategy(_tokens[i], _strategies[i]);
@@ -48,7 +78,7 @@ contract BigGreenButton {
 
     function setStrategies(address[] _tokens, address[] _strategies) public {
         require(
-            msg.sender == IController(controller).strategist() || msg.sender == IController(controller).governance(),
+            msg.sender == strategist || msg.sender == governance,
             "!strategist"
         );
         require(_tokens.length == _strategies.length, "!length");
