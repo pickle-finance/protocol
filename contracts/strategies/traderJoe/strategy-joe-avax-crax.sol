e80a700c1403d0c1c451412c7dcf599133a5ee66
Abigail <abigailtcameron@gmail.com>
2021-11-24 16:22:37 -0500

    updated avax-crax file with fixed dusting

--- contracts/strategies/traderJoe/strategy-joe-avax-crax.sol
+++ contracts/strategies/traderJoe/strategy-joe-avax-crax.sol
@@ -31,53 +31,49 @@ contract StrategyJoeAvaxCraxLp is StrategyJoeRushFarmBase {
     // **** State Mutations ****
 
     function harvest() public override onlyBenevolent {
-        // Anyone can harvest it at any given time.
-        // I understand the possibility of being frontrun
-        // But AVAX is a dark forest, and I wanna see how this plays out
-        // i.e. will be be heavily frontrunned?
-        //      if so, a new strategy will be deployed.
-
         // Collects Joe tokens
         IMasterChefJoeV2(masterChefJoeV3).deposit(poolId, 0);
 
-         //Take Avax Rewards    
-        uint256 _avax = address(this).balance;            //get balance of native Avax
-        if (_avax > 0) {                                 //wrap avax into ERC20
+        // Take Avax Rewards    
+        uint256 _avax = address(this).balance;              // get balance of native Avax
+        if (_avax > 0) {                                    // wrap avax into ERC20
             WAVAX(wavax).deposit{value: _avax}();
         }
         
         uint256 _wavax = IERC20(wavax).balanceOf(address(this));
         if (_wavax > 0) {
-             uint256 _keep2 = _wavax.mul(keep).div(keepMax);
-            uint256 _amount2 = _wavax.sub(_keep2).div(2);
+            uint256 _keep2 = _wavax.mul(keep).div(keepMax);
             if (_keep2 > 0){
                 _takeFeeWavaxToSnob(_keep2);
             }
+            
+            _wavax = IERC20(wavax).balanceOf(address(this));
 
-        //convert Avax Rewards
+            // Convert Avax Rewards
             IERC20(wavax).safeApprove(joeRouter, 0);
-            IERC20(wavax).safeApprove(joeRouter, _amount2);   
-            _swapTraderJoe(wavax, crax, _amount2);
+            IERC20(wavax).safeApprove(joeRouter, _wavax.div(2));   
+            _swapTraderJoe(wavax, crax, _wavax.div(2));
         }
 
         uint256 _joe = IERC20(joe).balanceOf(address(this));
         if (_joe > 0) {
             // 10% is sent to treasury
             uint256 _keep = _joe.mul(keep).div(keepMax);
-            uint256 _amount = _joe.sub(_keep).div(2);
             if (_keep > 0) {
                 _takeFeeJoeToSnob(_keep);
             }
-            IERC20(joe).safeApprove(joeRouter, 0);
-            IERC20(joe).safeApprove(joeRouter, _joe.sub(_keep));
 
-            _swapTraderJoe(joe, wavax, _amount);
-            _swapTraderJoe(joe, crax, _amount);
+            _joe = IERC20(joe).balanceOf(address(this));
+
+            IERC20(joe).safeApprove(joeRouter, 0);
+            IERC20(joe).safeApprove(joeRouter, _joe);
+
+            _swapTraderJoe(joe, wavax, _joe.div(2));
+            _swapTraderJoe(joe, crax, _joe.div(2));
         }
 
         // Adds in liquidity for AVAX/CRAX
         _wavax = IERC20(wavax).balanceOf(address(this));
-
         uint256 _crax = IERC20(crax).balanceOf(address(this));
 
         if (_wavax > 0 && _crax > 0) {
@@ -99,14 +95,21 @@ contract StrategyJoeAvaxCraxLp is StrategyJoeRushFarmBase {
             );
 
             // Donates DUST
-            IERC20(wavax).transfer(
-                IController(controller).treasury(),
-                IERC20(wavax).balanceOf(address(this))
-            );
-            IERC20(crax).safeTransfer(
-                IController(controller).treasury(),
-                IERC20(crax).balanceOf(address(this))
-            );
+            _wavax = IERC20(wavax).balanceOf(address(this));
+            _crax = IERC20(crax).balanceOf(address(this));
+            if (_wavax > 0){
+                IERC20(wavax).transfer(
+                    IController(controller).treasury(),
+                    _wavax
+                );
+            }
+            
+            if (_crax > 0){
+                IERC20(crax).safeTransfer(
+                    IController(controller).treasury(),
+                    _crax
+                );
+            }  
         }
 
         _distributePerformanceFeesAndDeposit();
