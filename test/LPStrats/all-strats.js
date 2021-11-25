32ac0aabc2bd7e192429d344789bfe7cc2624764
Abigail <abigailtcameron@gmail.com>
2021-11-24 20:14:32 -0500

    passes tests

--- test/LPStrats/all-strats.js
+++ test/LPStrats/all-strats.js
@@ -346,11 +346,11 @@ const tests = [
   //   controllerAddress: "",
   //   snowglobeAddress: "0xcC757081C972D0326de42875E0DA2c54af523622",
   // },
-  {
-    name: "JoeAvaxSnob",
-    controllerAddress: "",
-    snowglobeAddress: "0x8b2E1802A7E0E0c7e1EaE8A7c636058964e21047",
-  },
+  // {
+  //   name: "JoeAvaxSnob",
+  //   controllerAddress: "",
+  //   snowglobeAddress: "0x8b2E1802A7E0E0c7e1EaE8A7c636058964e21047",
+  // },
   // {
   //   name: "JoeAvaxXava",
   //   controllerAddress: "",
@@ -586,8 +586,13 @@ const tests = [
     //   controllerAddress: "",
     //   snowglobeAddress: "",
     // },
-      {
-      name: "JoeAvaxCrax",
+    // {
+    //   name: "JoeAvaxCrax",
+    //   controllerAddress: "",
+    //   snowglobeAddress: "",
+    // },
+    {
+      name: "JoeAvaxGro",
       controllerAddress: "",
       snowglobeAddress: "",
     },
