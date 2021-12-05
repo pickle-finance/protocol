f683fcad5ff319847f732698927bba79d9040fb6
Abigail <abigailtcameron@gmail.com>
2021-12-05 17:50:31 -0500

    order matters

--- contracts/strategies/axial/strategy-axial-aa3d.sol
+++ contracts/strategies/axial/strategy-axial-aa3d.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.6.7;
 
 import "../strategy-axial-3pool-base.sol";
+import "hardhat/console.sol";
 
 contract StrategyAxialAA3DLp is StrategyAxial3PoolBase {
     // stablecoins
@@ -22,9 +23,9 @@ contract StrategyAxialAA3DLp is StrategyAxial3PoolBase {
     public 
     StrategyAxial3PoolBase(
         swapLoan,
+        avai,
         mim,
-        usdcE,
-        avai, 
+        usdcE, 
         am3d_poolId,
         lp,
         _governance,
@@ -33,6 +34,50 @@ contract StrategyAxialAA3DLp is StrategyAxial3PoolBase {
         _timelock
 
     ){}
+
+    function _takeFeeOrcaToSnob(uint256 _keep) internal {
+        address[] memory path = new address[](3);
+        path[0] = orca;
+        path[1] = wavax;
+        path[2] = snob;
+        IERC20(orca).safeApprove(pangolinRouter, 0);
+        IERC20(orca).safeApprove(pangolinRouter, _keep);
+        _swapPangolinWithPath(path, _keep);
+        uint256 _snob = IERC20(snob).balanceOf(address(this));
+        uint256 _share = _snob.mul(revenueShare).div(revenueShareMax);
+        IERC20(snob).safeTransfer(feeDistributor, _share);
+        IERC20(snob).safeTransfer(
+            IController(controller).treasury(),
+            _snob.sub(_share)
+        );
+    }
+
+
+    function _orcaToToken(address _to) internal {
+
+        // if it's mim then orca to wavax using pangolin and then wavax to mim using joe. 
+        // if it's not mim then use pangolin with the three routing path. 
+        uint256 _wavax = IERC20(wavax).balanceOf(address(this));
+        uint256 _orca = IERC20(orca).balanceOf(address(this));
+        IERC20(orca).safeApprove(pangolinRouter, 0);
+        IERC20(orca).safeApprove(pangolinRouter, _orca);
+
+        if (_to == mim) {
+            _swapPangolin(orca, wavax, _orca); 
+
+            uint256 _postwavax = IERC20(wavax).balanceOf(address(this));
+            IERC20(wavax).safeApprove(joeRouter, 0);
+            IERC20(wavax).safeApprove(joeRouter, _postwavax.sub(_wavax));
+            _swapTraderJoe(wavax, _to, _postwavax.sub(_wavax));
+        }else {
+            address[] memory path = new address[](3);
+            path[0] = orca;
+            path[1] = wavax;
+            path[2] = _to;
+            
+            _swapPangolinWithPath(path, _orca);
+        }
+    }
 
     // **** State Mutations ****
 
@@ -59,6 +104,18 @@ contract StrategyAxialAA3DLp is StrategyAxial3PoolBase {
             _swapTraderJoe(axial, to, _axial);
         }
 
+        uint256 _orca = IERC20(orca).balanceOf(address(this));
+        if (_orca > 0) {
+            // x% is sent back to the rewards holder
+            // to be used to lock up in as veCRV in a future date
+            uint256 _keep = _orca.mul(keep).div(keepMax);
+            if (_keep > 0) {
+                _takeFeeOrcaToSnob(_keep);
+            }
+            
+            _orcaToToken(to);
+        }
+
         // Take Avax Rewards    
         uint256 _avax = address(this).balance;           //get balance of native Avax
         if (_avax > 0) {                                 //wrap avax into ERC20
@@ -80,29 +137,13 @@ contract StrategyAxialAA3DLp is StrategyAxial3PoolBase {
             _swapTraderJoe(wavax, to, _wavax);
         }
 
-        uint256 _orca = IERC20(orca).balanceOf(address(this));
-        if (_orca > 0) {
-            // x% is sent back to the rewards holder
-            // to be used to lock up in as veCRV in a future date
-            uint256 _keep = _orca.mul(keep).div(keepMax);
-            if (_keep > 0) {
-                _takeFeeOrcaToSnob(_keep);
-            }
-
-            _orca = IERC20(orca).balanceOf(address(this));
-
-            IERC20(orca).safeApprove(joeRouter, 0);
-            IERC20(orca).safeApprove(joeRouter, _orca);
-
-            _swapTraderJoe(orca, to, _orca);
-        }
-
-
         // Adds liquidity to axial's as4d or ac4d pool
         uint256 _to = IERC20(to).balanceOf(address(this));
+        
         if (_to > 0) {
             IERC20(to).safeApprove(flashLoan, 0);
             IERC20(to).safeApprove(flashLoan, _to);
+
             uint256[] memory liquidity = new uint256[](3);
 
             liquidity[0] = IERC20(pair1).balanceOf(address(this));
