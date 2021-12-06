fb6826de1c9ec4eaeda6130692ad29f0b734629b
big.wampa <79389347+bigwampa@users.noreply.github.com>
2021-12-05 16:37:08 -0800

    moving the funds

--- scripts/mend-deploy.js
+++ scripts/mend-deploy.js
@@ -5,12 +5,13 @@ async function main() {
   const pools = [
     {  
       name: "AxialAA3D",
-      strategy_addr: "0x8325C0bAe797fE52E5e5f8a13ab93f86a7D880aC",
+      // strategy_addr: "0x8325C0bAe797fE52E5e5f8a13ab93f86a7D880aC",
       snowglobe_addr: "0xb4281C75bab70734CDe886A9f6624385e88429CC",
-      setStrategy: true,
-      approveStrategy: true,
-      whitelist: true,
+      // setStrategy: true,
+      // approveStrategy: true,
+      // whitelist: true,
       setGlobe: true,
+      earn: true
     },
   ];
 
@@ -76,8 +77,8 @@ async function main() {
       lp = await Strategy.want();
       SnowGlobe = new ethers.Contract(pool.snowglobe_addr, snowglobe_ABI, deployer);
       console.log(`connected to ${snowglobe_name} at : ${SnowGlobe.address}`);
-      // pool.setGlobe=true;
-      // pool.addGauge=true;
+      pool.setGlobe=true;
+      pool.addGauge=true;
     }
   
     /* Set Globe */
