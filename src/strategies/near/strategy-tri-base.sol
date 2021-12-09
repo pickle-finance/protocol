f906b573844a1aaf6a920ccaf65f0e238854e74f
Larry the Cucumber <larrythecucumb321@gmail.com>
2021-12-08 17:38:43 -0800

    custom harvest for aurora's tiny limit

--- src/strategies/near/strategy-tri-base.sol
+++ src/strategies/near/strategy-tri-base.sol
@@ -16,6 +16,10 @@ abstract contract StrategyTriFarmBase is StrategyBase {
     address public token1;
     address rewardToken;
 
+    // How much TRI tokens to keep?
+    uint256 public keepTRI = 1000;
+    uint256 public constant keepTRIMax = 10000;
+
     uint256 public poolId;
     mapping(address => address[]) public swapRoutes;
 
@@ -35,6 +39,11 @@ abstract contract StrategyTriFarmBase is StrategyBase {
         poolId = _poolId;
         token0 = _token0;
         token1 = _token1;
+
+        IERC20(token0).approve(sushiRouter, uint256(-1));
+        IERC20(token1).approve(sushiRouter, uint256(-1));
+        IERC20(tri).approve(sushiRouter, uint256(-1));
+        IERC20(want).approve(miniChef, uint256(-1));
     }
 
     function balanceOfPool() public view override returns (uint256) {
@@ -45,24 +54,13 @@ abstract contract StrategyTriFarmBase is StrategyBase {
         return amount;
     }
 
-    function getHarvestable() external view returns (uint256, uint256) {
+    function getHarvestable() external view returns (uint256) {
         uint256 _pendingTri = IMiniChefTri(miniChef).pendingTri(
             poolId,
             address(this)
         );
-        IRewarder rewarder = IMiniChefTri(miniChef).rewarder(poolId);
-        (, uint256[] memory _rewardAmounts) = rewarder.pendingTokens(
-            poolId,
-            address(this),
-            0
-        );
 
-        uint256 _pendingReward;
-        if (_rewardAmounts.length > 0) {
-            _pendingReward = _rewardAmounts[0];
-        }
-
-        return (_pendingTri, _pendingReward);
+        return _pendingTri;
     }
 
     // **** Setters ****
@@ -70,8 +68,6 @@ abstract contract StrategyTriFarmBase is StrategyBase {
     function deposit() public override {
         uint256 _want = IERC20(want).balanceOf(address(this));
         if (_want > 0) {
-            IERC20(want).safeApprove(miniChef, 0);
-            IERC20(want).safeApprove(miniChef, _want);
             IMiniChefTri(miniChef).deposit(poolId, _want);
         }
     }
@@ -94,37 +90,65 @@ abstract contract StrategyTriFarmBase is StrategyBase {
 
     // **** State Mutations ****
 
+    function setKeepTRI(uint256 _keepTRI) external {
+        require(msg.sender == timelock, "!timelock");
+        keepTRI = _keepTRI;
+    }
+
     function harvest() public override onlyBenevolent {
+        harvestOne();
+        harvestTwo();
+        harvestThree();
+        harvestFour();
+    }
+
+    function harvestOne() public onlyBenevolent {
+        // Collects TRI tokens
+        IMiniChefTri(miniChef).harvest(poolId);
+        uint256 _tri = IERC20(tri).balanceOf(address(this));
+        uint256 _keepTRI = _tri.mul(keepTRI).div(keepTRIMax);
+
+        IERC20(tri).safeTransfer(IController(controller).treasury(), _keepTRI);
+    }
+
+    function harvestTwo() public onlyBenevolent {
         // Anyone can harvest it at any given time.
         // I understand the possibility of being frontrun
         // But ETH is a dark forest, and I wanna see how this plays out
         // i.e. will be be heavily frontrunned?
         //      if so, a new strategy will be deployed.
 
-        // Collects TRI tokens
-        IMiniChefTri(miniChef).harvest(poolId);
         uint256 _tri = IERC20(tri).balanceOf(address(this));
         if (_tri > 0) {
             uint256 toToken0 = _tri.div(2);
             uint256 toToken1 = _tri.sub(toToken0);
 
             if (swapRoutes[token0].length > 1) {
-                _swapSushiswapWithPath(swapRoutes[token0], toToken0);
+                UniswapRouterV2(sushiRouter).swapExactTokensForTokens(
+                    toToken0,
+                    0,
+                    swapRoutes[token0],
+                    address(this),
+                    now + 60
+                );
             }
             if (swapRoutes[token1].length > 1) {
-                _swapSushiswapWithPath(swapRoutes[token1], toToken1);
+                UniswapRouterV2(sushiRouter).swapExactTokensForTokens(
+                    toToken1,
+                    0,
+                    swapRoutes[token1],
+                    address(this),
+                    now + 60
+                );
             }
         }
+    }
 
+    function harvestThree() public onlyBenevolent {
         // Adds in liquidity for token0/token1
         uint256 _token0 = IERC20(token0).balanceOf(address(this));
         uint256 _token1 = IERC20(token1).balanceOf(address(this));
         if (_token0 > 0 && _token1 > 0) {
-            IERC20(token0).safeApprove(sushiRouter, 0);
-            IERC20(token0).safeApprove(sushiRouter, _token0);
-            IERC20(token1).safeApprove(sushiRouter, 0);
-            IERC20(token1).safeApprove(sushiRouter, _token1);
-
             UniswapRouterV2(sushiRouter).addLiquidity(
                 token0,
                 token1,
@@ -146,7 +170,9 @@ abstract contract StrategyTriFarmBase is StrategyBase {
                 IERC20(token1).balanceOf(address(this))
             );
         }
+    }
 
+    function harvestFour() public onlyBenevolent {
         // We want to get back Tri LP tokens
         _distributePerformanceFeesAndDeposit();
     }
