608d67212ca5b2e9f5acc32e64b37a852a68950b
amatureApe <amatureape@gmail.com>
2021-12-09 23:15:00 +0100

    removed second pad address declaration

--- src/strategies/near/nearpad/strategy-pad-pad-aurora-lp.sol
+++ src/strategies/near/nearpad/strategy-pad-pad-aurora-lp.sol
@@ -7,7 +7,6 @@ contract StrategyPadPadAuroraLp is StrategyNearPadFarmBase {
     uint256 public pad_aurora_poolid = 11;
     // Token addresses
     address public pad_aurora_lp = 0xac187A18f9DaB50506fc8111aa7E86F5F55DefE9;
-    address public pad = 0xDA2585430fEf327aD8ee44Af8F1f989a2A91A3d2;
     address public aurora = 0x8BEc47865aDe3B172A928df8f990Bc7f2A3b9f79;
 
     constructor(
