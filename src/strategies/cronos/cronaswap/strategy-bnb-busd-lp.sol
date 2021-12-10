b47ab27b661469fb5776d90d0101096af021a5d7
amatureApe <amatureape@gmail.com>
2021-12-10 19:39:19 +0100

    routing fix on bnb-busd and added btc-cro

--- src/strategies/cronos/cronaswap/strategy-bnb-busd-lp.sol
+++ src/strategies/cronos/cronaswap/strategy-bnb-busd-lp.sol
@@ -10,6 +10,7 @@ contract StrategyCronaBnbBusdLp is StrategyCronaFarmBase {
     address public bnb_busd_lp = 0xe8B18116040acf83D6e1f873375adF61103AB45c;
     address public busd = 0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8;
     address public bnb = 0xfA9343C3897324496A05fC75abeD6bAC29f8A40f;
+    address public usdc = 0xc21223249CA28397B4B6541dfFaEcC539BfF0c59;
 
     constructor(
         address _governance,
---
+++ src/strategies/cronos/cronaswap/strategy-btc-cro-lp.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-crona-farm-base.sol";
+
+contract StrategyCronaBtcCroLp is StrategyCronaFarmBase {
+    uint256 public btc_cro_poolId = 6;
+
+    // Token addresses
+    address public btc_cro_lp = 0x8232aA9C3EFf79cd845FcDa109B461849Bf1Be83;
+    address public btc = 0x062E66477Faf219F25D27dCED647BF57C3107d52;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyCronaFarmBase(
+            btc,
+            cro,
+            btc_cro_poolId,
+            btc_cro_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {
+        uniswapRoutes[cro] = [crona, cro];
+        uniswapRoutes[btc] = [crona, cro, btc];
+    }
+
+    // **** Views ****
+
+    function getName() external pure override returns (string memory) {
+        return "StrategyCronaBtcCroLp";
+    }
+}
