f172914dc8ba8aab950c58498aa26ac34bd8ec02
amatureApe <amatureape@gmail.com>
2021-12-30 19:12:58 +0200

    added sushi-sps-eth strategy

---
+++ src/strategies/sushiswap/masterchefV2/strategy-sushi-sps-eth-lp.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-masterchefv2-base.sol";
+
+contract StrategySushiSpsEthLp is StrategyMasterchefV2FarmBase {
+    uint256 public sushi_sps_eth_poolId = 34;
+
+    address public sushi_sps_eth_lp =
+        0x1b803Af96F37AE51Fa753E13E1D2E372dFA17791;
+    address public sps = 0x00813E3421E1367353BfE7615c7f7f133C89df74;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyMasterchefV2FarmBase(
+            sushi_sps_eth_poolId,
+            sushi_sps_eth_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** State Mutations ****
+
+    function harvest() public override onlyBenevolent {
+        // Anyone can harvest it at any given time.
+        // I understand the possibility of being frontrun
+        // But ETH is a dark forest, and I wanna see how this plays out
+        // i.e. will be be heavily frontrunned?
+        //      if so, a new strategy will be deployed.
+
+        // Collects Sushi and SPS tokens
+        IMasterchefV2(masterChef).harvest(poolId, address(this));
+
+        uint256 _sps = IERC20(sps).balanceOf(address(this));
+        if (_sps > 0) {
+            uint256 _amount = _sps.div(2);
+            IERC20(sps).safeApprove(sushiRouter, 0);
+            IERC20(sps).safeApprove(sushiRouter, _amount);
+            _swapSushiswap(sps, weth, _amount);
+        }
+
+        uint256 _sushi = IERC20(sushi).balanceOf(address(this));
+        if (_sushi > 0) {
+            uint256 _amount = _sushi.div(2);
+            IERC20(sushi).safeApprove(sushiRouter, 0);
+            IERC20(sushi).safeApprove(sushiRouter, _sushi);
+
+            _swapSushiswap(sushi, weth, _amount);
+            _swapSushiswap(sushi, sps, _amount);
+        }
+
+        // Adds in liquidity for SPS/WETH
+        uint256 _weth = IERC20(weth).balanceOf(address(this));
+
+        _sps = IERC20(sps).balanceOf(address(this));
+
+        if (_weth > 0 && _sps > 0) {
+            IERC20(weth).safeApprove(sushiRouter, 0);
+            IERC20(weth).safeApprove(sushiRouter, _weth);
+
+            IERC20(sps).safeApprove(sushiRouter, 0);
+            IERC20(sps).safeApprove(sushiRouter, _sps);
+
+            UniswapRouterV2(sushiRouter).addLiquidity(
+                sps,
+                weth,
+                _sps,
+                _weth,
+                0,
+                0,
+                address(this),
+                now + 60
+            );
+
+            // Donates DUST
+            IERC20(weth).transfer(
+                IController(controller).treasury(),
+                IERC20(weth).balanceOf(address(this))
+            );
+            IERC20(sps).safeTransfer(
+                IController(controller).treasury(),
+                IERC20(sps).balanceOf(address(this))
+            );
+        }
+
+        _distributePerformanceFeesAndDeposit();
+    }
+
+    // **** Views ****
+
+    function getName() external pure override returns (string memory) {
+        return "StrategySushiSpsEthLp";
+    }
+}
