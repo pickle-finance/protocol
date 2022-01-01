e6025c0fdd08abb1240f38cfd8e645355da07565
amatureApe <amatureape@gmail.com>
2022-01-01 09:48:56 +0200

    added near-luna auroraswap strategy

---
+++ src/strategies/near/auroraswap/strategy-brl-near-luna-lp.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-brl-base.sol";
+
+contract StrategyBrlNearLunaLp is StrategyBrlFarmBase {
+    uint256 public near_luna_poolid = 7;
+    // Token addresses
+    address public near_luna_lp = 0x388D5EE199aC8dAD049B161b57487271Cd787941;
+    address public luna = 0xC4bdd27c33ec7daa6fcfd8532ddB524Bf4038096;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyNearPadFarmBase(
+            near,
+            luna,
+            near_luna_poolid,
+            near_luna_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {
+        swapRoutes[near] = [brl, near];
+        swapRoutes[luna] = [brl, near, luna];
+    }
+
+    // **** Views ****
+
+    function getName() external pure override returns (string memory) {
+        return "StrategyBrlNearLunaLp";
+    }
+}
