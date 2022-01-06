c490d3b3ea59ceaf48a54f3c93ca100680ef6277
amatureApe <amatureape@gmail.com>
2022-01-07 00:02:06 +0200

    fixed auroraswap usdt-usdc strategy

--- src/strategies/near/auroraswap/strategy-brl-usdt-usdc-lp.sol
+++ src/strategies/near/auroraswap/strategy-brl-usdt-usdc-lp.sol
@@ -6,7 +6,7 @@ import "../strategy-brl-base.sol";
 contract StrategyBrlUsdtUsdcLp is StrategyBrlFarmBase {
     uint256 public usdt_near_poolid = 2;
     // Token addresses
-    address public usdt_near_lp = 0xEc538fAfaFcBB625C394c35b11252cef732368cd;
+    address public usdt_usdc_lp = 0xEc538fAfaFcBB625C394c35b11252cef732368cd;
     address public usdt = 0x4988a896b1227218e4A686fdE5EabdcAbd91571f;
     address public usdc = 0xB12BFcA5A55806AaF64E99521918A4bf0fC40802;
 
@@ -19,9 +19,9 @@ contract StrategyBrlUsdtUsdcLp is StrategyBrlFarmBase {
         public
         StrategyBrlFarmBase(
             usdt,
-            near,
-            usdt_near_poolid,
-            usdt_near_lp,
+            usdc,
+            usdt_usdc_poolid,
+            usdt_usdc_lp,
             _governance,
             _strategist,
             _controller,
