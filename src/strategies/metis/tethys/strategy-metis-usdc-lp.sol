ffaa470175ed5f9db39f25d9b83e4cb9a8578176
amatureApe <amatureape@gmail.com>
2022-01-10 14:38:54 +0200

    added tethys strategies

---
+++ src/strategies/metis/tethys/strategy-metis-usdc-lp.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-tethys-base.sol";
+
+contract StrategyTethysMetisLp is StrategyTethysFarmLPBase {
+    // Token addresses
+    uint256 public metis_usdc_poolId = 2;
+    address public metis_usdc_lp = 0xDd7dF3522a49e6e1127bf1A1d3bAEa3bc100583B;
+    address public usdc = 0xEA32A96608495e54156Ae48931A7c20f0dcc1a21;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyTethysFarmLPBase(
+            metis_usdc_lp,
+            metis_usdc_poolId,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {
+        uniswapRoutes[metis] = [tethys, metis];
+        uniswapRoutes[usdc] = [tethys, metis, usdc];
+    }
+
+    // **** Views ****
+
+    function getName() external pure override returns (string memory) {
+        return "StrategyTethysMetisLp";
+    }
+}
