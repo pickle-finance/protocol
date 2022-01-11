2cb1d0a9f16dc448dce9180b5c7568b76afb5ca9
amatureApe <amatureape@gmail.com>
2022-01-11 13:48:16 +0200

    address fix

--- src/strategies/metis/nettswap/strategy-nett-usdc-lp.sol
+++ src/strategies/metis/nettswap/strategy-nett-usdc-lp.sol
@@ -6,7 +6,7 @@ import "../strategy-netswap-base.sol";
 contract StrategyNettNettUsdcLp is StrategyNettFarmLPBase {
     uint256 public nett_usdc_poolid = 1;
     // Token addresses
-    address public nett_usdc_lp = 0x7D02ab940d7dD2B771e59633bBC1ed6EC2b99Af1;
+    address public nett_usdc_lp = 0x0724d37522585E87d27C802728E824862Dc72861;
     address public usdc = 0xEA32A96608495e54156Ae48931A7c20f0dcc1a21;
 
     constructor(
