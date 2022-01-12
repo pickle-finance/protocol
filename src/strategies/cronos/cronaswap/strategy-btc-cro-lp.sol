92ed8db0597fd1fda88ce0a931bc6c45caf6547b
amatureApe <amatureape@gmail.com>
2022-01-12 14:59:06 +0200

    strategy fix - swap tokens

--- src/strategies/cronos/cronaswap/strategy-btc-cro-lp.sol
+++ src/strategies/cronos/cronaswap/strategy-btc-cro-lp.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.6.7;
 
 import "../strategy-crona-farm-base.sol";
 
-contract StrategyCronaCroBtcLp is StrategyCronaFarmBase {
-    uint256 public cro_btc_poolId = 5;
+contract StrategyCronaBtcCroLp is StrategyCronaFarmBase {
+    uint256 public btc_cro_poolId = 5;
 
     // Token addresses
-    address public cro_btc_lp = 0xb4684F52867dC0dDe6F931fBf6eA66Ce94666860;
+    address public btc_cro_lp = 0xb4684F52867dC0dDe6F931fBf6eA66Ce94666860;
     address public btc = 0x062E66477Faf219F25D27dCED647BF57C3107d52;
 
     constructor(
@@ -18,10 +18,10 @@ contract StrategyCronaCroBtcLp is StrategyCronaFarmBase {
     )
         public
         StrategyCronaFarmBase(
-            cro,
             btc,
-            cro_btc_poolId,
-            cro_btc_lp,
+            cro,
+            btc_cro_poolId,
+            btc_cro_lp,
             _governance,
             _strategist,
             _controller,
@@ -35,6 +35,6 @@ contract StrategyCronaCroBtcLp is StrategyCronaFarmBase {
     // **** Views ****
 
     function getName() external pure override returns (string memory) {
-        return "StrategyCronaCroBtcLp";
+        return "StrategyCronaBtcCroLp";
     }
 }
