bd21ca0d3404f54484cb01ee4dbe491dda605b0b
Cipio <Cipio@Pickle.finance>
2022-02-12 00:24:10 -0600

    Revert controller v6 to original. Update to controller v7.

--- src/controller-v7.sol
+++ src/controller-v7.sol
@@ -2,20 +2,26 @@
 
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
+import "./lib/Initializable.sol";
 
 import "./lib/erc20.sol";
 import "./lib/safe-math.sol";
 
 import "./interfaces/jar.sol";
 import "./interfaces/jar-converter.sol";
+import "./interfaces/strategy.sol";
 import "./interfaces/strategyv2.sol";
+import "./interfaces/onesplit.sol";
 import "./interfaces/converter.sol";
 import "./interfaces/univ3/IUniswapV3Pool.sol";
 
-contract ControllerV6 {
+contract ControllerV6 is Initializable {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
+
+    address public constant burn = 0x000000000000000000000000000000000000dEaD;
+    address public onesplit = 0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E;
 
     address public governance;
     address public strategist;
@@ -23,23 +29,19 @@ contract ControllerV6 {
     address public treasury;
     address public timelock;
 
+    uint256 public convenienceFee = 100;
+    uint256 public constant convenienceFeeMax = 100000;
+
     mapping(address => address) public jars;
     mapping(address => address) public strategies;
+    mapping(address => mapping(address => address)) public converters;
     mapping(address => mapping(address => bool)) public approvedStrategies;
+    mapping(address => bool) public approvedJarConverters;
 
-    constructor(
-        address _governance,
-        address _strategist,
-        address _timelock,
-        address _devfund,
-        address _treasury
-    ) public {
-        governance = _governance;
-        strategist = _strategist;
-        timelock = _timelock;
-        devfund = _devfund;
-        treasury = _treasury;
-    }
+    uint256 public split = 500;
+    uint256 public constant max = 10000;
+
+    function initialize() public initializer {}
 
     function setDevFund(address _devfund) public {
         require(msg.sender == governance, "!governance");
@@ -82,6 +84,17 @@ contract ControllerV6 {
         approvedStrategies[_pool][_strategy] = false;
     }
 
+    function setConvenienceFee(uint256 _convenienceFee) external {
+        require(msg.sender == timelock, "!timelock");
+        convenienceFee = _convenienceFee;
+    }
+
+    // in case of strategy stuck and if we need to relink the new strategy
+    function removeJar(address _pool) public {
+        require(msg.sender == strategist || msg.sender == governance, "!strategist");
+        jars[_pool] = address(0);
+    }
+
     // in case of strategy stuck and if we need to relink the new strategy
     function removeStrategy(address _pool) public {
         require(msg.sender == strategist || msg.sender == governance, "!strategist");
@@ -99,6 +112,25 @@ contract ControllerV6 {
         strategies[_pool] = _strategy;
     }
 
+    function setSplit(uint256 _split) public {
+        require(msg.sender == governance, "!governance");
+        require(_split <= max, "numerator cannot be greater than denominator");
+        split = _split;
+    }
+
+    function setOneSplit(address _onesplit) public {
+        require(msg.sender == governance, "!governance");
+        onesplit = _onesplit;
+    }
+
+    function getUpperTick(address _pool) external view returns (int24) {
+        return IStrategyV2(strategies[_pool]).tick_upper();
+    }
+
+    function getLowerTick(address _pool) external view returns (int24) {
+        return IStrategyV2(strategies[_pool]).tick_lower();
+    }
+
     function earn(
         address _pool,
         uint256 _token0Amount,
@@ -108,13 +140,31 @@ contract ControllerV6 {
         address _want = IStrategyV2(_strategy).pool();
         require(_want == _pool, "pool address is different");
 
-        IERC20(IUniswapV3Pool(_pool).token0()).safeTransfer(_strategy, _token0Amount);
-        IERC20(IUniswapV3Pool(_pool).token1()).safeTransfer(_strategy, _token1Amount);
+        if (_token0Amount > 0) IERC20(IUniswapV3Pool(_pool).token0()).safeTransfer(_strategy, _token0Amount);
+        if (_token1Amount > 0) IERC20(IUniswapV3Pool(_pool).token1()).safeTransfer(_strategy, _token1Amount);
         IStrategyV2(_strategy).deposit();
+    }
+
+    function earn(address _token, uint256 _amount) public {
+        address _strategy = strategies[_token];
+        address _want = IStrategy(_strategy).want();
+        if (_want != _token) {
+            address converter = converters[_token][_want];
+            IERC20(_token).safeTransfer(converter, _amount);
+            _amount = Converter(converter).convert(_strategy);
+            IERC20(_want).safeTransfer(_strategy, _amount);
+        } else {
+            IERC20(_token).safeTransfer(_strategy, _amount);
+        }
+        IStrategy(_strategy).deposit();
     }
 
     function liquidityOf(address _pool) external view returns (uint256) {
         return IStrategyV2(strategies[_pool]).liquidityOf();
+    }
+
+    function balanceOf(address _token) external view returns (uint256) {
+        return IStrategy(strategies[_token]).balanceOf();
     }
 
     function withdrawAll(address _pool) public returns (uint256 a0, uint256 a1) {
@@ -137,6 +187,11 @@ contract ControllerV6 {
         (a0, a1) = IStrategyV2(strategies[_pool]).withdraw(_amount);
     }
 
+    function withdrawReward(address _token, uint256 _reward) public {
+        require(msg.sender == jars[_token], "!jar");
+        IStrategy(strategies[_token]).withdrawReward(_reward);
+    }
+
     function onERC721Received(
         address,
         address,
@@ -144,6 +199,47 @@ contract ControllerV6 {
         bytes memory
     ) public pure returns (bytes4) {
         return this.onERC721Received.selector;
+    }
+
+    function getExpectedReturn(
+        address _strategy,
+        address _token,
+        uint256 parts
+    ) public view returns (uint256 expected) {
+        uint256 _balance = IERC20(_token).balanceOf(_strategy);
+        address _want = IStrategy(_strategy).want();
+        (expected, ) = OneSplitAudit(onesplit).getExpectedReturn(_token, _want, _balance, parts, 0);
+    }
+
+    // Only allows to withdraw non-core strategy tokens ~ this is over and above normal yield
+    function yearn(
+        address _strategy,
+        address _token,
+        uint256 parts
+    ) public {
+        require(msg.sender == strategist || msg.sender == governance, "!governance");
+        // This contract should never have value in it, but just incase since this is a public call
+        uint256 _before = IERC20(_token).balanceOf(address(this));
+        IStrategy(_strategy).withdraw(_token);
+        uint256 _after = IERC20(_token).balanceOf(address(this));
+        if (_after > _before) {
+            uint256 _amount = _after.sub(_before);
+            address _want = IStrategy(_strategy).want();
+            uint256[] memory _distribution;
+            uint256 _expected;
+            _before = IERC20(_want).balanceOf(address(this));
+            IERC20(_token).safeApprove(onesplit, 0);
+            IERC20(_token).safeApprove(onesplit, _amount);
+            (_expected, _distribution) = OneSplitAudit(onesplit).getExpectedReturn(_token, _want, _amount, parts, 0);
+            OneSplitAudit(onesplit).swap(_token, _want, _amount, _expected, _distribution, 0);
+            _after = IERC20(_want).balanceOf(address(this));
+            if (_after > _before) {
+                _amount = _after.sub(_before);
+                uint256 _treasury = _amount.mul(split).div(max);
+                earn(_want, _amount.sub(_treasury));
+                IERC20(_want).safeTransfer(treasury, _treasury);
+            }
+        }
     }
 
     function _execute(address _target, bytes memory _data) internal returns (bytes memory response) {
