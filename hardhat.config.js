f2dee6efa2a1d0925e4593a7f81fa3105af8f658
Cipio <Cipio@Pickle.finance>
2022-02-15 18:22:01 -0600

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,15 +31,6 @@ module.exports = {
           },
         },
       },
-      {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
     ],
   },
   networks: {
