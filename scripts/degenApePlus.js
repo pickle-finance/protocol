89f4fd093cca7d18bcc5c36d08b64f3baf07ddb5
amatureApe <amatureApe@gmail.com>
2022-02-15 16:46:22 -0800

    add updates

--- scripts/degenApePlus.js
+++ scripts/degenApePlus.js
@@ -29,16 +29,10 @@ const contracts = [
 ];
 
 const testedStrategies = [
-  "0xA6b01164af308d74eD593e24637275ee26Cf9531", "0x20b515d6fA1a248e92350d449286B8D258d91C19", "0xbe9e4d2902f23B83c9d04c1780C09809af5E7b3F", "0x2722930172C38420a4A0Aa7af67C316ebD845Be4", "0x62e02D2E56A18C5DCD5bE447D30D04C9800519E8", "0x767ef1887A71734A1F5198b2bE6dA9c32293ca5e",
+  "0xaD5b7F1Af5f58b17185C24D2E6C011D49EAA5F4c",
+  "0x767ef1887A71734A1F5198b2bE6dA9c32293ca5e",
+  "0x08751fAC1dA7D063daF6a2a6B5D6770F2f5517f7",
 ];
-
-const jarAndStrat ={};
-
-// Script configs
-const sleepToggle = true;
-const sleepTime = 10000;
-const recallTime = 60000
-const callAttempts = 3;
 
 // Functions
 const sleep = async (ms, active=true) => {
@@ -69,27 +63,19 @@ const verifyContracts = async (strategies) => {
 }
 
 const deployAndTest = async () => {
-// Script configs
-  const sleepToggle = true;
-  const sleepTime = 15000;
-  const recallTime = 60000
-  const callAttempts = 3;
-
-// References
-  const txRefs ={};
-  const allReports = [];
-
   const executeTx = async (calls, tx, fn, ...args) => {
     await sleep(sleepTime, sleepToggle);
-    recall(executeTx, calls, tx, fn, ...args);
+    if (!txRefs[tx]) {recall(executeTx, calls, tx, fn, ...args)}
     try {
-      txRefs[tx] = await fn(...args);
-      console.log(txRefs);
-      if (tx === strategy || tx === jar) {
-        await tx.deployTransaction.wait();
-      } else {
-        await tx.wait();
+      if(!txRefs[tx]) {
+        txRefs[tx] = await fn(...args);
+        if (tx === strategy || tx === jar) {
+          await tx.deployTransaction.wait();
+        } else {
+          await tx.wait();
+        }
       }
+      // console.log(`Transaction receipt: ${txRefs[tx]}`)
     } catch (e) {
       console.error(e);
       if (calls > 0) {
@@ -105,30 +91,29 @@ const deployAndTest = async () => {
 
   for (const contract of contracts) {
     const StrategyFactory = await ethers.getContractFactory(contract);
-    // console.log("" + StrategyFactory.deploy);
     const PickleJarFactory = await ethers.getContractFactory("src/pickle-jar.sol:PickleJar");
     const Controller = await ethers.getContractAt("src/controller-v4.sol:ControllerV4", controller);
-    const currentContract = contract.substring(contract.lastIndexOf(":") + 1);
+    txRefs[name] = contract.substring(contract.lastIndexOf(":") + 1);
 
     try {
 // Deploy Strategy contract
-      console.log(`Deploying ${currentContract}...`);
+      console.log(`Deploying ${txRefs[name]}...`);
       let strategy;
       await executeTx(callAttempts, strategy, StrategyFactory.deploy, governance, strategist, controller, timelock);
-      console.log(`✔️ Strategy deployed at: ${strategy.address}`);
+      console.log(`✔️ Strategy deployed at: ${txRefs[strategy].address}`);
 
 // Get Want
-      const want = await txRefs[strategy].want();
+      txRefs[want] = await txRefs[strategy].want();
 
 // Deploy PickleJar contract
       let jar;
-      await executeTx(callAttempts, txRefs[jar], PickleJarFactor.deploy, want, governance, timelock, controller);
+      await executeTx(callAttempts, jar, PickleJarFactor.deploy, txRefs[want], governance, timelock, controller);
       console.log(`✔️ PickleJar deployed at: ${txRefs[jar].address}`);
 
 // Log Want
-      console.log(`Want address is: ${want}`);
+      console.log(`Want address is: ${txRefs[want]}`);
       console.log(`Approving want token for deposit...`);
-      const wantContract = await ethers.getContractAt("ERC20", want);
+      const wantContract = await ethers.getContractAt("ERC20", txRefs[want]);
 
 // Approve Want
       let approveTx;
@@ -138,7 +123,7 @@ const deployAndTest = async () => {
 
 // Approve Strategy
       let approveStratTx;
-      await executeTx(callAttempts, approveStratTx, Controller.approveStrategy, want, txRefs[strategy].address);
+      await executeTx(callAttempts, approveStratTx, Controller.approveStrategy, txRefs[want], txRefs[strategy].address);
       console.log(`Strategy Approved!`)
 
 // Set Jar
@@ -148,7 +133,7 @@ const deployAndTest = async () => {
 
 // Set Strategy
       let setStratTx;
-      await executeTx(callAttempts, setStratTx, Controller.setStrategy, want, strategy.address);
+      await executeTx(callAttempts, setStratTx, Controller.setStrategy, txRefs[want], strategy.address);
       console.log(`Strategy Set!`)
       console.log(`✔️ Controller params all set!`);
 
@@ -170,27 +155,29 @@ const deployAndTest = async () => {
       let harvestTx;
       await executeTx(callAttempts, harvestTx, txRefs[strategy].harvest);
 
-      const ratio = await txRefs[jar].getRatio();
+      txRefs[ratio] = await txRefs[jar].getRatio();
 
-      if (ratio.gt(BigNumber.from(parseEther("1")))) {
-        console.log(`✔️ Harvest was successful, ending ratio of ${ratio.toString()}`);
-        testedStrategies.push({contract: currentContract, address: txRefs[strategy].address})
+      if (txRefs[ratio].gt(BigNumber.from(parseEther("1")))) {
+        console.log(`✔️ Harvest was successful, ending ratio of ${txRefs[ratio].toString()}`);
+        testedStrategies.push(txRefs[strategy].address)
       } else {
-        console.log(`❌ Harvest failed, ending ratio of ${ratio.toString()}`);
+        console.log(`❌ Harvest failed, ending ratio of ${txRefs[ratio].toString()}`);
       }
 // Script Report
       const report =
       `
       Jar Info -
-      name: ${currentContract}
-      want: ${want}
+      name: ${txRefs[name]}
+      want: ${txRefs[want]}
       picklejar: ${txRefs[jar].address}
       strategy: ${txRefs[strategy].address}
-      ratio: ${ratio.toString()}
+      ratio: ${txRefs[ratio].toString()}
       `;
 
-      console.log(report)
-      allReports.push(report)
+      // console.log(txRefs);
+      // console.log(report);
+
+      allReports.push(report);
     } catch (e) {
       console.log(`Oops something went wrong...`);
       console.error(e);
@@ -202,12 +189,15 @@ const deployAndTest = async () => {
       Here's the full report -
     ----------------------------
     ${allReports.join('\n')}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    (>'-')> <('-'<) ^('-')^ v('-')v
+    '''''''''''''''''''''''''''''
     `
   );
 };
 
 const main = async () => {
-  await deployAndTest();
+  // await deployAndTest();
   await verifyContracts(testedStrategies);
 };
 
