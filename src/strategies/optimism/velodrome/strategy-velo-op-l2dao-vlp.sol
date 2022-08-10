8e0a34083590fdebab4a47311450c071c4613fd0
mard <mistermard@protonmail.com>
2022-08-10 21:53:43 +0800

    add velo op-l2dao strategy

---
+++ src/strategies/optimism/velodrome/strategy-velo-op-l2dao-vlp.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+pragma experimental ABIEncoderV2;
+
+import "./strategy-velo-base.sol";
+
+contract StrategyVeloOpL2daoVlp is StrategyVeloBase {
+    // Addresses
+    address private _lp = 0xfc77e39De40E54F820E313039207DC850E4C9E60;
+    address private _gauge = 0xB4d9036B81b9B6A7De1C70887c29938eC8df6048;
+    address private constant l2dao = 0xd52f94DF742a6F4B4C8b033369fE13A41782Bf44;
+    address private constant op = 0x4200000000000000000000000000000000000042;
+
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyVeloBase(
+            _lp,
+            _gauge,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {
+        isStablePool = false;
+
+        // token0 route
+        nativeToTokenRoutes[op].push(RouteParams(native, op, false));
+
+        // token1 route
+        nativeToTokenRoutes[l2dao].push(RouteParams(native, op, false));
+        nativeToTokenRoutes[l2dao].push(RouteParams(op, l2dao, false));
+    }
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyVeloOpL2daoVlp";
+    }
+}
