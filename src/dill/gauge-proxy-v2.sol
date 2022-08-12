408ecbf7885dfee31cdeb306562129c2ec64f93a
Aditya Rout <adi.b20612@gmail.com>
2022-08-12 12:31:20 +0530

    fix: minor fix

--- src/dill/gauge-proxy-v2.sol
+++ src/dill/gauge-proxy-v2.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.1;
 import "protocol/node_modules/@openzeppelin/contracts/utils/Address.sol";
 import "protocol/node_modules/@openzeppelin/contracts/interfaces/IERC20.sol";
 import "protocol/node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "protocol/node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface IGaugeMiddleware {
     function addGauge(address _token, address _governance)
